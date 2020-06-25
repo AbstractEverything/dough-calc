3556f5471621a4907524c4bf3dc6f44501217e98
AbstractEverything <ingonyama26@googlemail.com>
2020-06-25 11:33:06 +0100

    Use totalOverallDry method instead lf totalOfType

--- src/components/sidebar/BakersPercentagesList.vue
+++ src/components/sidebar/BakersPercentagesList.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="pt-4 mb-4 border-t border-dashed border-orange-300">
     <h3 class="text-lg font-bold">Bakers %:</h3>
-    <div v-if="totalOfType('dry') > 0">
+    <div v-if="totalOverallDry > 0">
       <ul>
         <li v-for="item in allOfTypeNot('dry')" :key="item.name">
-          {{ item.name }}:<span class="float-right"><strong>{{ round((item.value / totalOfType('dry')) * 100, 1) }}</strong><em>%</em></span>
+          {{ item.name }}:<span class="float-right"><strong>{{ round((item.value / totalOverallDry) * 100, 1) }}</strong><em>%</em></span>
         </li>
       </ul>
     </div>
@@ -12,6 +12,7 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 import helpers from '../../helpers'
 
 export default {
@@ -25,6 +26,11 @@ export default {
     round(value, decimals) {
       return helpers.round(value, decimals)
     }
+  },
+  computed: {
+    ...mapGetters([
+      'totalOverallDry',
+    ])
   }
 }
 </script>
